71976969e97d5f622a7d06014e98a7a855502b3d
mxro <mxro@nowhere.com>
2015-07-12 18:09:44 +1000

    Moving projects to java delight Autocommit Sun Jul 12 18:09:44 EST 2015

--- src/test/java/de/mxro/client/tests/TestCreateAndStop.java
+++ src/test/java/de/mxro/client/tests/TestCreateAndStop.java
@@ -1,23 +1,238 @@
 package de.mxro.client.tests;
 
-/* @JUnit */@SuppressWarnings("all")
+import de.mxro.client.BasicClient;
+import de.mxro.client.jre.Clients;
+import de.mxro.metrics.jre.Metrics;
+import de.oehme.xtend.junit.JUnit;
+import delight.async.properties.PropertyNode;
+import delight.async.properties.PropertyOperation;
+import delight.async.properties.jre.Properties;
+import delight.functional.Success;
+import delight.log.jre.Logs;
+import delight.promise.Promise;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure0;
+import org.hamcrest.Matcher;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.internal.ArrayComparisonFailure;
+
+@JUnit
+@SuppressWarnings("all")
 public class TestCreateAndStop {
-  /* @Test
-   */public void test() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method logs is undefined for the type TestCreateAndStop"
-      + "\nThe method or field Logs is undefined for the type TestCreateAndStop"
-      + "\nThe method metrics is undefined for the type TestCreateAndStop"
-      + "\nThe method or field Metrics is undefined for the type TestCreateAndStop"
-      + "\nThe method state is undefined for the type TestCreateAndStop"
-      + "\nThe method or field Properties is undefined for the type TestCreateAndStop"
-      + "\nThe method stop is undefined for the type TestCreateAndStop"
-      + "\nrecord cannot be resolved"
-      + "\nstring cannot be resolved"
-      + "\nrecord cannot be resolved"
-      + "\nincrement cannot be resolved"
-      + "\nrecord cannot be resolved"
-      + "\nset cannot be resolved"
-      + "\nget cannot be resolved");
+  @Test
+  public void test() {
+    BasicClient client = Clients.create();
+    PropertyNode _logs = client.logs();
+    PropertyOperation<String> _string = Logs.string(this, "I was there.");
+    _logs.<String>record(_string);
+    PropertyNode _metrics = client.metrics();
+    PropertyOperation<Long> _increment = Metrics.increment("counter");
+    _metrics.<Long>record(_increment);
+    PropertyNode _state = client.state();
+    PropertyOperation<Object> _set = Properties.set("123", "456");
+    _state.<Object>record(_set);
+    Promise<Success> _stop = client.stop();
+    _stop.get();
+  }
+  
+  private static void assertArrayEquals(final Object[] expecteds, final Object[] actuals) {
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final byte[] expecteds, final byte[] actuals) {
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final char[] expecteds, final char[] actuals) {
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final short[] expecteds, final short[] actuals) {
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final int[] expecteds, final int[] actuals) {
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final long[] expecteds, final long[] actuals) {
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final String message, final Object[] expecteds, final Object[] actuals) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final String message, final byte[] expecteds, final byte[] actuals) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final String message, final char[] expecteds, final char[] actuals) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final String message, final short[] expecteds, final short[] actuals) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final String message, final int[] expecteds, final int[] actuals) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final String message, final long[] expecteds, final long[] actuals) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals);
+  }
+  
+  private static void assertArrayEquals(final double[] expecteds, final double[] actuals, final double delta) {
+    Assert.assertArrayEquals(expecteds, actuals, delta);
+  }
+  
+  private static void assertArrayEquals(final float[] expecteds, final float[] actuals, final float delta) {
+    Assert.assertArrayEquals(expecteds, actuals, delta);
+  }
+  
+  private static void assertArrayEquals(final String message, final double[] expecteds, final double[] actuals, final double delta) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals, delta);
+  }
+  
+  private static void assertArrayEquals(final String message, final float[] expecteds, final float[] actuals, final float delta) throws ArrayComparisonFailure {
+    Assert.assertArrayEquals(message, expecteds, actuals, delta);
+  }
+  
+  private static void assertEquals(final Object expected, final Object actual) {
+    Assert.assertEquals(expected, actual);
+  }
+  
+  private static void assertEquals(final long expected, final long actual) {
+    Assert.assertEquals(expected, actual);
+  }
+  
+  private static void assertEquals(final String message, final Object expected, final Object actual) {
+    Assert.assertEquals(message, expected, actual);
+  }
+  
+  private static void assertEquals(final String message, final long expected, final long actual) {
+    Assert.assertEquals(message, expected, actual);
+  }
+  
+  private static void assertEquals(final double expected, final double actual, final double delta) {
+    Assert.assertEquals(expected, actual, delta);
+  }
+  
+  private static void assertEquals(final float expected, final float actual, final float delta) {
+    Assert.assertEquals(expected, actual, delta);
+  }
+  
+  private static void assertEquals(final String message, final double expected, final double actual, final double delta) {
+    Assert.assertEquals(message, expected, actual, delta);
+  }
+  
+  private static void assertEquals(final String message, final float expected, final float actual, final float delta) {
+    Assert.assertEquals(message, expected, actual, delta);
+  }
+  
+  private static void assertFalse(final boolean condition) {
+    Assert.assertFalse(condition);
+  }
+  
+  private static void assertFalse(final String message, final boolean condition) {
+    Assert.assertFalse(message, condition);
+  }
+  
+  private static void assertNotEquals(final Object first, final Object second) {
+    Assert.assertNotEquals(first, second);
+  }
+  
+  private static void assertNotEquals(final long first, final long second) {
+    Assert.assertNotEquals(first, second);
+  }
+  
+  private static void assertNotEquals(final String message, final Object first, final Object second) {
+    Assert.assertNotEquals(message, first, second);
+  }
+  
+  private static void assertNotEquals(final String message, final long first, final long second) {
+    Assert.assertNotEquals(message, first, second);
+  }
+  
+  private static void assertNotEquals(final double first, final double second, final double delta) {
+    Assert.assertNotEquals(first, second, delta);
+  }
+  
+  private static void assertNotEquals(final String message, final double first, final double second, final double delta) {
+    Assert.assertNotEquals(message, first, second, delta);
+  }
+  
+  private static void assertNotNull(final Object object) {
+    Assert.assertNotNull(object);
+  }
+  
+  private static void assertNotNull(final String message, final Object object) {
+    Assert.assertNotNull(message, object);
+  }
+  
+  private static void assertNotSame(final Object unexpected, final Object actual) {
+    Assert.assertNotSame(unexpected, actual);
+  }
+  
+  private static void assertNotSame(final String message, final Object unexpected, final Object actual) {
+    Assert.assertNotSame(message, unexpected, actual);
+  }
+  
+  private static void assertNull(final Object object) {
+    Assert.assertNull(object);
+  }
+  
+  private static void assertNull(final String message, final Object object) {
+    Assert.assertNull(message, object);
+  }
+  
+  private static void assertSame(final Object expected, final Object actual) {
+    Assert.assertSame(expected, actual);
+  }
+  
+  private static void assertSame(final String message, final Object expected, final Object actual) {
+    Assert.assertSame(message, expected, actual);
+  }
+  
+  private static <T extends Object> void assertThat(final T actual, final Matcher<? super T> matcher) {
+    Assert.<T>assertThat(actual, matcher);
+  }
+  
+  private static <T extends Object> void assertThat(final String reason, final T actual, final Matcher<? super T> matcher) {
+    Assert.<T>assertThat(reason, actual, matcher);
+  }
+  
+  private static void assertTrue(final boolean condition) {
+    Assert.assertTrue(condition);
+  }
+  
+  private static void assertTrue(final String message, final boolean condition) {
+    Assert.assertTrue(message, condition);
+  }
+  
+  private static void fail() {
+    Assert.fail();
+  }
+  
+  private static void fail(final String message) {
+    Assert.fail(message);
+  }
+  
+  private static <T extends Object, U extends T> void operator_doubleArrow(final T actual, final U expected) {
+    Assert.assertEquals(expected, actual);
+  }
+  
+  private static <T extends Exception> void isThrownBy(final Class<T> expected, final Procedure0 block) {
+    try {
+    	block.apply();
+    	Assert.fail("Expected a " + expected.getName());
+    } catch (Exception e) {
+    	Class<?> actual = e.getClass();
+    	Assert.assertTrue(
+    		"Expected a " + expected.getName() + " but got " + actual.getName(), 
+    		expected.isAssignableFrom(actual)
+    	);
+    }
   }
 }
